e71155efabc0887cfc65a58a9c5bda9e716bd204
root <root@DESKTOP-R1BEV7I.localdomain>
2026-04-16 15:20:49 +0800

    fix: resolve merge conflicts and remove duplicate migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2026_04_16_010000) do
+ActiveRecord::Schema[8.0].define(version: 2026_04_16_020000) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -326,11 +326,11 @@ ActiveRecord::Schema[8.0].define(version: 2026_04_16_010000) do
     t.string "status"
     t.string "token"
     t.datetime "completed_at"
-    t.string "document_status"
-    t.string "signature_status"
     t.jsonb "metadata"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "signature_status"
+    t.string "document_status"
     t.index ["booking_id"], name: "index_pre_checkins_on_booking_id"
   end
 
