62ef14a67db7928771aaa2b29d22182a3aa81e17
Nurahfezan Nordin <nurahfezanbinnordin@gmail.com>
2026-05-20 23:01:50 +0800

    feat: implement granular folio permissions and enhanced action UI
    - Add migration for specialized folio permissions (refund, reversal, adjustment, void)
    - Introduce seed service to assign granular roles to hotel staff
    - Update folio view and actions partial to respect new granular permission system

--- spec/services/hotel_ops/seed_account_roles_spec.rb
+++ spec/services/hotel_ops/seed_account_roles_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe HotelOps::SeedAccountRoles do
   let(:account) { create(:account) }
 
   before do
-    %w[manage_account manage_hotel_profile manage_room_types manage_rates manage_inventory view_bookings manage_bookings view_guest_phone manage_guest_arrival view_audit_logs export_audit_logs manage_users manage_room_status post_charges post_folio_transactions view_reports view_payouts manage_requests manage_night_audit].each do |slug|
+    %w[manage_account manage_hotel_profile manage_room_types manage_rates manage_inventory view_bookings manage_bookings view_guest_phone manage_guest_arrival view_audit_logs export_audit_logs manage_users manage_room_status post_charges post_folio_charges post_folio_payments execute_folio_refunds post_folio_adjustments post_folio_corrections post_folio_write_offs view_reports view_payouts manage_requests manage_night_audit].each do |slug|
       Permission.find_or_create_by!(slug: slug) do |permission|
         permission.name = slug.humanize
       end
@@ -18,6 +18,17 @@ RSpec.describe HotelOps::SeedAccountRoles do
 
     owner = Role.find_by!(account: account, slug: "hotel_owner")
     expect(owner.role_permissions).not_to be_empty
-    expect(owner.permissions.pluck(:slug)).to include("post_folio_transactions")
+    expect(owner.permissions.pluck(:slug)).to include(
+      "post_folio_charges",
+      "post_folio_payments",
+      "execute_folio_refunds",
+      "post_folio_adjustments",
+      "post_folio_corrections",
+      "post_folio_write_offs"
+    )
+
+    front_desk = Role.find_by!(account: account, slug: "front_desk")
+    expect(front_desk.permissions.pluck(:slug)).to include("post_folio_charges", "post_folio_payments")
+    expect(front_desk.permissions.pluck(:slug)).not_to include("execute_folio_refunds", "post_folio_write_offs", "post_folio_corrections")
   end
 end
