db29501def6ae756a66f8a18d877e41601177c15
Evy-art <evybivvy09@gmail.com>
2026-07-01 15:15:17 +0800

    feat: show time remaining until checkout in room view

--- app/controllers/hotel_portal/bookings/board_controller.rb
+++ app/controllers/hotel_portal/bookings/board_controller.rb
@@ -6,16 +6,22 @@ module HotelPortal
       before_action :authorize_booking_timeline_board!
 
       def index
+        @board_view_type = parse_board_view_type
         @start_date = parse_start_date
         @board_days = parse_board_days
-        @board_layout = parse_board_layout
-        @board_view_type = parse_board_view_type
+
+        if @board_view_type == "room"
+          @board_days = 1
+        end
+
         @room_types = current_hotel.room_types.order(:name)
         @selected_room_type_id = params[:room_type_id].presence
         if @board_view_type == "room" && @selected_room_type_id.blank?
           @selected_room_type_id = @room_types.first&.id&.to_s
         end
+        @board_layout = parse_board_layout
         @board_query_params = { days: @board_days, layout: @board_layout, view_type: @board_view_type, room_type_id: @selected_room_type_id }
+
         @rate_plan_names = RatePlan.joins(:room_type).where(room_types: { hotel_id: current_hotel.id }).distinct.pluck(:name)
         @selected_rate_plan_name = params.dig(:filters, :rate_plan_name) || @rate_plan_names.first
         filters = params[:filters].respond_to?(:to_unsafe_h) ? params[:filters].to_unsafe_h : (params[:filters] || {})
